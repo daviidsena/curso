ba54570340081ae01e40b54f0ce8e607bb2dca96
daviidsena <daviidsena@gmail.com>
2022-09-08 22:14:06 -0300

    Chore: add faker dependency

--- src/main/java/br/com/senac/inicializacao/Init.java
+++ src/main/java/br/com/senac/inicializacao/Init.java
@@ -1,12 +1,22 @@
 package br.com.senac.inicializacao;
 
+import java.util.Iterator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import com.github.javafaker.Faker;
+
 import br.com.senac.entity.Aluno;
+import br.com.senac.entity.Curso;
+import br.com.senac.entity.Professor;
+import br.com.senac.entity.Turma;
 import br.com.senac.service.AlunoService;
+import br.com.senac.service.CursoService;
+import br.com.senac.service.ProfessorService;
+import br.com.senac.service.TurmaService;
 
 @Component
 public class Init implements ApplicationListener<ContextRefreshedEvent>{
@@ -14,16 +24,24 @@ public class Init implements ApplicationListener<ContextRefreshedEvent>{
 	@Autowired
 	private AlunoService alunoService;
 	
+	@Autowired
+	private TurmaService turmaService;
+	
+	@Autowired
+	private CursoService cursoService;
+	
+	@Autowired
+	private ProfessorService professorService;
+	
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
-		Aluno aluno1 = new Aluno();
-		aluno1.setNome("Lucas");
-		alunoService.salvar(aluno1);
-
-		Aluno aluno4 = alunoService.buscarPorId(1);
+		Faker faker = new Faker();
 		
-		System.out.println(aluno4.getNome());
-		
-		alunoService.deletarPorId(1);
+		for (int i = 0; i < 10; i++) {
+			alunoService.salvar(new Aluno(faker.name().firstName()));
+			turmaService.salvar(new Turma(faker.code().asin()));
+			cursoService.salvar(new Curso(faker.name().firstName()));
+			professorService.salvar(new Professor(faker.name().firstName()));
+		}
 	}
 }
