08b5659decbbdef44387237813936352014261af
David Sena <daviidsena@gmail.com>
2022-09-15 21:24:13 -0300

    feat: add controller professor

--- src/main/java/br/com/senac/controller/ProfessorController.java
+++ src/main/java/br/com/senac/controller/ProfessorController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.senac.entity.Professor;
 import br.com.senac.service.ProfessorService;
 
 @Controller
@@ -27,5 +29,27 @@ public class ProfessorController {
 	public String excluirProfessor(@PathVariable("id") Integer id) {
 		professorService.deletarPorId(id);
 		return "redirect:/professor/";
+	}
+	
+	@GetMapping("/cadastrar/")
+	public ModelAndView cadastrarProfessor() {
+		ModelAndView mv = new ModelAndView("professor/cadastrar");
+		mv.addObject("professor", new Professor());
+		mv.addObject("title", "Cadastrar Professor");
+		return mv;
+	}
+	
+	@PostMapping("/cadastrar/")
+	public String cadastrarProfessorPost(Professor professor) {
+		professorService.salvar(professor);
+		return "redirect:/professor/";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public ModelAndView editar(@PathVariable("id") Integer id) {
+		ModelAndView mv = new ModelAndView("professor/cadastrar");
+		mv.addObject("professor", professorService.buscarPorId(id));
+		mv.addObject("title", "Editar Professor");
+		return mv;
 	}
 }	
