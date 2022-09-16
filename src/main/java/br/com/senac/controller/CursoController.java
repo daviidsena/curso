320fbb87f91cb5ca29c720fa421054272a14e1a4
David Sena <daviidsena@gmail.com>
2022-09-15 21:22:45 -0300

    feat: add edit and register curso

--- src/main/java/br/com/senac/controller/CursoController.java
+++ src/main/java/br/com/senac/controller/CursoController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.senac.entity.Curso;
 import br.com.senac.service.CursoService;
 
 @Controller
@@ -27,5 +29,27 @@ public class CursoController {
 	public String excluirCurso(@PathVariable("id") Integer id) {
 		cursoService.deletarPorId(id);
 		return "redirect:/curso/";
+	}
+	
+	@GetMapping("/cadastrar/")
+	public ModelAndView cadastrarCurso() {
+		ModelAndView mv = new ModelAndView("curso/cadastrar");
+		mv.addObject("curso", new Curso());
+		mv.addObject("title", "Cadastrar Curso");
+		return mv;
+	}
+	
+	@PostMapping("/cadastrar/")
+	public String cadastrarCursoPost(Curso curso) {
+		cursoService.salvar(curso);
+		return "redirect:/curso/";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public ModelAndView editar(@PathVariable("id") Integer id) {
+		ModelAndView mv = new ModelAndView("curso/cadastrar");
+		mv.addObject("curso", cursoService.buscarPorId(id));
+		mv.addObject("title", "Editar Curso");
+		return mv;
 	}
 }	
